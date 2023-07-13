8ea477a92bbd43c8bf9bc48cc2a111c0b159cde0
blacklite <blacklite21`@gmail.com>
2023-07-13 23:40:46 +0700

    Начал создавать категории, но нужно еще подумать.
    Будет работать так.
    С самого начала есть одна категория и туда можно добавлять задачи. Перемещая задачу по категориям мы меняем ее "Статус". Статусом будет являться название категории

--- js/main.ts
+++ js/main.ts
@@ -1,57 +1,105 @@
-class Main
-{
-	public tasks : Task[];
-	public container : JQuery;
-	public list : JQuery;
-
-	constructor()
-	{
-		this.tasks = [];
-		this.container = $('div.tasks');
-		const btn = $('<div/>', { class: 'create', text: 'Новая задача' }).on('click', () => this.createTask());
-		this.list = $('<div/>', { class: 'list', html: `<span class="empty">Создайте свою первую задачу</span>` }).appendTo(this.container);
-		this.container.append(btn);
-	}
-
-	public createTask()
-	{
-		const container = ccOverlay.overlay();
-		ccOverlay.open(() => true, null);
-
-		container.append(`<div class="container">
-			<div class="add_task">
-				<input type="text" placeholder="НАЗВАНИЕ ЗАДАЧИ">
-				<div class="button">Создать</div>
-			</div>
-		</div>`);
-
-		container.find('div.add_task').on('click', () => false);
-		container.find('div.button').on('click', () => {
-			new Task(container.find('input').val().toString(), this.list);
-			this.list.find('span.empty').addClass('hide');
-		});
-
-		// const task = new Task(container.find('input').val().toString(), this.list)
-	}
-}
+// class Main
+// {
+// 	public tasks : Task[];
+// 	public container : JQuery;
+// 	public list : JQuery;
+//
+// 	constructor()
+// 	{
+// 		this.tasks = [];
+// 		this.container = $('div.tasks');
+// 		const btn = $('<div/>', { class: 'button create', text: 'Новая задача' }).on('click', () => this.createTask());
+// 		this.list = $('<div/>', { class: 'list' }).appendTo(this.container);
+// 		this.container.append(btn);
+// 	}
+//
+// 	public createTask()
+// 	{
+// 		const container = ccOverlay.overlay();
+// 		ccOverlay.open(() => true, null);
+//
+// 		container.append(`<div class="container">
+// 			<div class="add_task">
+// 				<input type="text" placeholder="НАЗВАНИЕ ЗАДАЧИ">
+// 				<div class="button">Создать</div>
+// 			</div>
+// 		</div>`);
+//
+// 		const input = container.find('input');
+// 		const button = container.find('div.button').addClass('disabled');
+//
+// 		input.on('input', () => {
+// 			if (input.val().toString().trim() == '') button.off('click').addClass('disabled');
+// 			else
+// 			{
+// 				button.removeClass('disabled');
+// 				button.off('click').on('click', () => new Task(container.find('input').val().toString(), this.list));
+// 			}
+// 		});
+//
+// 		container.find('div.add_task').on('click', () => false);
+//
+// 		// const task = new Task(container.find('input').val().toString(), this.list)
+// 	}
+// }
 
 class Task
 {
-	constructor(text: string, container: JQuery)
-	{
-		container.append(`<label>
-			<span>
-				<input type="checkbox">
-				<span>${text}</span>
-			</span>
-			<span></span>
-		</label>`);
+	public constructor() {}
 
-		ccOverlay.close()
+	public static create() : JQuery
+	{
+		return $(`<div class="task">
+			<label><span><input type="text" placeholder="Название задачи"/></span></label>
+			<span class="delete"></span>
+		</div>`)
 	}
+}
+
+class Categories
+{
+	public items : JQuery[] = [];
+	public container : JQuery = null;
+
+	public constructor()
+	{
+		// this.container = $('body > div > div.content');
+		// this.container.append(``)
+	}
+
+	public add(item: JQuery) { this.items.push(item); }
 }
 
 class Category
 {
+	public tasks : JQuery[] = [];
+	public container : JQuery = null;
+	public category : JQuery = null;
 
+	public constructor()
+	{
+		this.container = $('body > div > div.content');
+	}
+
+	protected create()
+	{
+		this.category = $('<div/>', { class: 'category' });
+
+		this.category.append(`
+			<div class="title"><input type="text" placeholder="Название категории"/></div>
+			<div class="tasks"><div><div class="button">Новая задача</div></div></div>
+		`);
+
+		this.category.find('.button').on('click', () => {
+			const task = Task.create();
+			this.category.find('div.tasks').append(task);
+			this.tasks.push(task);
+		});
+
+
+
+		this.container.append(this.category)
+	}
 }
+
+let TDCategories = new Category();
--- main.js
+++ main.js
@@ -1,43 +1,88 @@
 "use strict";
-class Main {
-    constructor() {
-        this.tasks = [];
-        this.container = $('div.tasks');
-        const btn = $('<div/>', { class: 'create', text: 'Новая задача' }).on('click', () => this.createTask());
-        this.list = $('<div/>', { class: 'list', html: `<span class="empty">Создайте свою первую задачу</span>` }).appendTo(this.container);
-        this.container.append(btn);
-    }
-    createTask() {
-        const container = ccOverlay.overlay();
-        ccOverlay.open(() => true, null);
-        container.append(`<div class="container">
-			<div class="add_task">
-				<input type="text" placeholder="НАЗВАНИЕ ЗАДАЧИ">
-				<div class="button">Создать</div>
-			</div>
+// class Main
+// {
+// 	public tasks : Task[];
+// 	public container : JQuery;
+// 	public list : JQuery;
+//
+// 	constructor()
+// 	{
+// 		this.tasks = [];
+// 		this.container = $('div.tasks');
+// 		const btn = $('<div/>', { class: 'button create', text: 'Новая задача' }).on('click', () => this.createTask());
+// 		this.list = $('<div/>', { class: 'list' }).appendTo(this.container);
+// 		this.container.append(btn);
+// 	}
+//
+// 	public createTask()
+// 	{
+// 		const container = ccOverlay.overlay();
+// 		ccOverlay.open(() => true, null);
+//
+// 		container.append(`<div class="container">
+// 			<div class="add_task">
+// 				<input type="text" placeholder="НАЗВАНИЕ ЗАДАЧИ">
+// 				<div class="button">Создать</div>
+// 			</div>
+// 		</div>`);
+//
+// 		const input = container.find('input');
+// 		const button = container.find('div.button').addClass('disabled');
+//
+// 		input.on('input', () => {
+// 			if (input.val().toString().trim() == '') button.off('click').addClass('disabled');
+// 			else
+// 			{
+// 				button.removeClass('disabled');
+// 				button.off('click').on('click', () => new Task(container.find('input').val().toString(), this.list));
+// 			}
+// 		});
+//
+// 		container.find('div.add_task').on('click', () => false);
+//
+// 		// const task = new Task(container.find('input').val().toString(), this.list)
+// 	}
+// }
+class Task {
+    constructor() { }
+    static create() {
+        return $(`<div class="task">
+			<label><span><input type="text" placeholder="Название задачи"/></span></label>
+			<span class="delete"></span>
 		</div>`);
-        container.find('div.add_task').on('click', () => false);
-        container.find('div.button').on('click', () => {
-            new Task(container.find('input').val().toString(), this.list);
-            this.list.find('span.empty').addClass('hide');
-        });
-        // const task = new Task(container.find('input').val().toString(), this.list)
     }
 }
-class Task {
-    constructor(text, container) {
-        container.append(`<label>
-			<span>
-				<input type="checkbox">
-				<span>${text}</span>
-			</span>
-			<span></span>
-		</label>`);
-        ccOverlay.close();
+class Categories {
+    constructor() {
+        this.items = [];
+        this.container = null;
+        // this.container = $('body > div > div.content');
+        // this.container.append(``)
     }
+    add(item) { this.items.push(item); }
 }
 class Category {
+    constructor() {
+        this.tasks = [];
+        this.container = null;
+        this.category = null;
+        this.container = $('body > div > div.content');
+    }
+    create() {
+        this.category = $('<div/>', { class: 'category' });
+        this.category.append(`
+			<div class="title"><input type="text" placeholder="Название категории"/></div>
+			<div class="tasks"><div><div class="button">Новая задача</div></div></div>
+		`);
+        this.category.find('.button').on('click', () => {
+            const task = Task.create();
+            this.category.find('div.tasks').append(task);
+            this.tasks.push(task);
+        });
+        this.container.append(this.category);
+    }
 }
+let TDCategories = new Category();
 var Components;
 (function (Components) {
     class Overlay {
